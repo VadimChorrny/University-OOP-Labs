06433696f4c7110bac84ec2a3b6a55567cb44226
VadimChorrny <chorrny228@gmail.com>
2023-11-06 08:49:04 +0200

    lab 5 upload

---
+++ Lab 5/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Lab_5.Services;
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("Лабораторна робота №5 ");
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("Виконав студент групи ІПЗ-22, Чорний Вадим Олександрович\n");
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine(
+    "Різниця між 4 та 5 лаборатоною роботою, в тому що в 5 лабораторній роботі використовується поліморфізм\n" +
+    "та демонструється робота віртуальних методів, коли наперед невідомо який об'єкт буде створено (на етапі компіляції)\n");
+
+Console.ResetColor();
+
+// Створення об'єкта системи трьох лінійних нерівностей
+LinearInequalitySystem system = new ThreeLinearInequalitySystem();
+system.SetCoefficients(1, 2, 2);
+system.DisplayCoefficients();
+
+double[] userVector = { 2, 3, 4 }; // задовільниний вектор
+double[] userVector_false = { 2, 3, 1 }; // незадовільниний вектор
+
+// Використання віртуального методу IsSatisfied, навіть коли ми маємо посилання на базовий клас,
+// демонструє поліморфізм, оскільки викликати буде перевизначений метод з похідного класу,
+// інкапсулюючи логіку перевірки системи нерівностей, навіть коли об'єкт змінюється
+bool isSatisfied = system.IsSatisfied(userVector);
+
+if (isSatisfied)
+{
+    Console.Write("Система трьох нерівностей:");
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(" Задовільниний вектор");
+    Console.ResetColor();
+}
+else
+{
+    Console.Write("Система трьох лінійних нерівностей:");
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(" Незадовільниний вектор");
+    Console.ResetColor();
+}
---
+++ Lab 5/Services/LinearInequalitySystem.cs
@@ -0,0 +1,60 @@
+namespace Lab_5.Services;
+
+// Базовий клас "Система лінійних нерівностей"
+public class LinearInequalitySystem
+{
+    protected double[] coefficients;
+
+    protected LinearInequalitySystem(int variableCount)
+    {
+        coefficients = new double[variableCount];
+    }
+
+    // Метод для встановлення коефіцієнтів нерівностей
+    public void SetCoefficients(params double[] coeffs)
+    {
+        if (coeffs.Length == coefficients.Length)
+        {
+            for (int i = 0; i < coeffs.Length; i++)
+            {
+                coefficients[i] = coeffs[i];
+            }
+        }
+        else
+        {
+            Console.WriteLine("Кількість коефіцієнтів не відповідає кількості змінних.");
+        }
+    }
+
+    // Метод для виведення коефіцієнтів на екран
+    public void DisplayCoefficients()
+    {
+        Console.Write("Коефіцієнти: ");
+        foreach (double coeff in coefficients)
+        {
+            Console.Write(coeff + " ");
+        }
+
+        Console.WriteLine();
+    }
+
+    // Віртуальний метод для перевірки, чи введений вектор задовольняє системі нерівностей
+    public virtual bool IsSatisfied(double[] vector)
+    {
+        if (vector.Length != coefficients.Length)
+        {
+            Console.WriteLine("Розмірність вектора не відповідає розмірності системи нерівностей.");
+            return false;
+        }
+
+        for (int i = 0; i < coefficients.Length; i++)
+        {
+            if (vector[i] < coefficients[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
